8181c36f2a5edacbc46d7ea91c7870f13cbb0a93
Yuxia <rainycheng1988@163.com>
2017-02-22 11:05:45 +1100

    myhash test result

--- mySGX/sgx/enclave_encl1/untrusted/hash.py
+++ mySGX/sgx/enclave_encl1/untrusted/hash.py
@@ -1,7 +1,8 @@
 import os,sys
 
-hashpower = (21,22,23,24,25)
+hashpower = (20,21,22,23,24,25,26)
 
-for i in range(0,5):
+for i in range(0,7):
     command = "./myhash " + str(hashpower[i]) + " " + str(2**hashpower[i])
+    print command 
     os.system(command)
